592d40a28b66af6eeb6cc6dd8593e9d21e150ee0
22mahmoud <mahmoudss1997@gmail.com>
2020-09-27 14:10:35 +0200

    setup basic server

---
+++ src/schema.ts
@@ -0,0 +1,48 @@
+import { nexusSchemaPrisma } from "nexus-plugin-prisma/schema";
+import { intArg, makeSchema, objectType, stringArg } from '@nexus/schema'
+
+const Post = objectType({
+  name: 'Post',
+  definition(t) {
+    t.model.id()
+    t.model.title()
+    t.model.content()
+  },
+})
+
+const Query = objectType({
+  name: 'Query',
+  definition(t) {
+    t.crud.post();
+    t.crud.posts();
+  },
+})
+
+const Mutation = objectType({
+  name: 'Mutation',
+  definition(t) {
+    t.crud.createOnePost();
+  },
+})
+
+export const schema = makeSchema({
+  types: [Query, Mutation, Post],
+  plugins: [nexusSchemaPrisma({ experimentalCRUD: true })],
+  outputs: {
+    schema: __dirname + '/../schema.graphql',
+    typegen: __dirname + '/generated/nexus.ts',
+  },
+  typegenAutoConfig: {
+    contextType: 'Context.Context',
+    sources: [
+      {
+        source: '@prisma/client',
+        alias: 'prisma',
+      },
+      {
+        source: require.resolve('./context'),
+        alias: 'Context',
+      },
+    ],
+  },
+})
---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import { ApolloServer } from 'apollo-server';
+
+import { schema } from './schema';
+import { createContext } from './context';
+
+const server = new ApolloServer({ schema, context: createContext });
+
+server.listen({ port: 4000  }, () => {
+  console.log(`🚀 Server ready at: http://localhost:4000\n⭐️ `);
+});
+
