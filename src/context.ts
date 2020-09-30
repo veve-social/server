44daac02577d7aff273ea0b264dad59c36b2f040
22mahmoud <mahmoudss1997@gmail.com>
2020-09-30 20:31:41 +0200

    add redis to the context

--- src/context.ts
+++ src/context.ts
@@ -1,5 +1,8 @@
 import { Request, Response } from 'express';
 import { PrismaClient } from '@prisma/client';
+import Redis from 'ioredis';
+
+import { redis } from './config/redis';
 
 export const prisma = new PrismaClient();
 
@@ -7,6 +10,7 @@ export interface Context {
   prisma: PrismaClient;
   req: Request;
   res: Response;
+  redis: Redis.Redis;
 }
 
 export function createContext({
@@ -16,5 +20,5 @@ export function createContext({
   req: Request;
   res: Response;
 }): Context {
-  return { prisma, req, res };
+  return { prisma, req, res, redis };
 }
